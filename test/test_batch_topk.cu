fbd3238ceab51fbf23bf0361239fd79dbb05b382
fire-emblem <123421156@qq.com>
2026-04-01 14:32:13 +0800

    cuda: add first nibble selector for topk50

--- test/test_batch_topk.cu
+++ test/test_batch_topk.cu
@@ -12,6 +12,7 @@
 #include "../src/dispatch_policy.cuh"
 #include "../src/final_topk50.cuh"
 #include "../src/radix_boundary_select.cuh"
+#include "../src/radix_first_nibble_select.cuh"
 #include "../src/radix_histogram.cuh"
 #include "../src/radix_select_state.cuh"
 
@@ -266,7 +267,7 @@ radix_topk::SegmentSelectState simulate_first_nibble_select(
     state.prefix_mask = 0xffffu;
     state.selected_count = bounded_seg_len;
     state.live_count = 0;
-    state.boundary_digit = 255;
+    state.boundary_digit = 15;
     return state;
   }
 
